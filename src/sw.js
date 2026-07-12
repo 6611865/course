75647e8d4548d08f5a6ca573619ac6d38603f6d8
Course Dev <dev@course-schedule.local>
2026-07-12 15:56:05 +0800

    chore: SW缓存版本升到v19，修复Safari加载旧缓存问题

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v18';
+const CACHE_VERSION = 'v19';
 const CACHE_NAME = 'course-table-' + CACHE_VERSION;
 const FONT_CACHE_NAME = 'course-table-fonts-' + CACHE_VERSION;
 const PRECACHE_URLS = [
